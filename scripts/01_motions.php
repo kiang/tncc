8ae46ed72d6f7bb05f587a1fb9aec01f07d93939
kiang <kiange@gmail.com>
2024-06-17 13:09:11 +0800

    new way to get motions

---
+++ scripts/01_motions.php
@@ -0,0 +1,34 @@
+<?php
+$bastPath = dirname(__DIR__);
+//https://bill.tncc.gov.tw/NoPaperMeeting_TNCC/api/WEB014_GetProposalList.ashx?topage=1&pagerow=100&grdno=1
+
+for ($grdno = 1; $grdno <= 4; $grdno++) {
+    $grdPath = $bastPath . '/motions/' . $grdno;
+    if (!file_exists($grdPath)) {
+        mkdir($grdPath, 0777, true);
+    }
+    $page = 1;
+    $pageAll = 1;
+    $pageAllDone = false;
+    while ($page <= $pageAll) {
+        $pageFile = $grdPath . '/page_' . $page . '.json';
+        $url = "https://bill.tncc.gov.tw/NoPaperMeeting_TNCC/api/WEB014_GetProposalList.ashx?topage={$page}&pagerow=100&grdno={$grdno}";
+        if (!file_exists($pageFile)) {
+            $c = json_decode(file_get_contents($url));
+            file_put_contents($pageFile, json_encode($c, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+        }
+        $json = json_decode(file_get_contents($pageFile), true);
+        foreach ($json as $item) {
+            if (false === $pageAllDone) {
+                $pageAllDone = true;
+                $pageAll = $item['PagesAll'];
+            }
+            $motionFile = $grdPath . '/case_' . $item['ProNo'] . '.json';
+            if (!file_exists($motionFile)) {
+                $c = json_decode(file_get_contents("https://bill.tncc.gov.tw/NoPaperMeeting_TNCC/Api/WEB016_GetOneProposal.ashx?prono={$item['ProNo']}"));
+                file_put_contents($motionFile, json_encode($c, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+            }
+        }
+        $page++;
+    }
+}
