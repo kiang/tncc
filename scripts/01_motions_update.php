0b5fc17b0ac246972b60eb1a6230bd4ccce0f9b3
kiang <kiange@gmail.com>
2024-06-19 11:26:44 +0800

    sort attachments by filenames

--- scripts/01_motions_update.php
+++ scripts/01_motions_update.php
@@ -10,6 +10,13 @@ if (!file_exists($grdPath)) {
 $page = 1;
 $pageAll = 1;
 $pageAllDone = false;
+function cmp($a, $b)
+{
+    if (!isset($a['FileName']) || $a['FileName'] == $b['FileName']) {
+        return 0;
+    }
+    return ($a['FileName'] < $b['FileName']) ? -1 : 1;
+}
 while ($page <= $pageAll) {
     $pageFile = $grdPath . '/page_' . $page . '.json';
     $url = "https://bill.tncc.gov.tw/NoPaperMeeting_TNCC/api/WEB014_GetProposalList.ashx?topage={$page}&pagerow=100&grdno={$grdno}";
@@ -22,7 +29,13 @@ while ($page <= $pageAll) {
             $pageAll = $item['PagesAll'];
         }
         $motionFile = $grdPath . '/case_' . $item['ProNo'] . '.json';
-        $c = json_decode(file_get_contents("https://bill.tncc.gov.tw/NoPaperMeeting_TNCC/Api/WEB016_GetOneProposal.ashx?prono={$item['ProNo']}"));
+        $c = json_decode(file_get_contents("https://bill.tncc.gov.tw/NoPaperMeeting_TNCC/Api/WEB016_GetOneProposal.ashx?prono={$item['ProNo']}"), true);
+        foreach ($c as $k => $v) {
+            if (is_array($c[$k])) {
+                usort($c[$k], "cmp");
+            }
+        }
+
         file_put_contents($motionFile, json_encode($c, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
     }
     $page++;
